fa970361a3b775c110d8019f8ad6c06384dbc65f
MattEBoy <teo1997@gmail.com>
2021-02-10 16:02:41 -0700

    Add project files.

---
+++ Restaurants/Models/Restaurant.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Restaurants.Models
+{
+    public class Restaurant
+    {
+        [Required]
+        public int Rank { get; set; }
+        [Required]
+        public String RestaurantName { get; set; }
+        public String FavDish { get; set; }
+        [Required]
+        public String Address { get; set; }
+        public String RestaurantPhone { get; set; }
+        public String Link { get; set; }
+
+        public static Restaurant GetRestaurant()
+        {
+
+            return new Restaurant[](null);
+        }
+    }
+}
